9827f2ff0c481fca6f4e27280160c06ac549b771
行健 张 <826091979@qq.com>
2019-01-13 21:17:37 +0800

    change

--- CGProject/CGProject/game.cpp
+++ CGProject/CGProject/game.cpp
@@ -342,14 +342,14 @@ void Game::Render()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
-	glm::vec3 lightPos(10*glm::cos(glfwGetTime()), 10.0f, 10*glm::sin(glfwGetTime()));
+	glm::vec3 lightPos(100*glm::cos(glfwGetTime()), 100.0f, 100*glm::sin(glfwGetTime()));
 	// 1. render depth of scene to texture (from light's perspective)
 	// --------------------------------------------------------------
 	glm::mat4 lightProjection, lightView;
 	glm::mat4 lightSpaceMatrix;
 	float near_plane = 0.1f, far_plane = 5000.0f;
-	lightProjection = glm::perspective(glm::radians(45.0f), (GLfloat)SHADOW_WIDTH / (GLfloat)SHADOW_HEIGHT, near_plane, far_plane); // note that if you use a perspective projection matrix you'll have to change the light position as the current light position isn't enough to reflect the whole scene
-	//lightProjection = glm::ortho(-100.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+	//lightProjection = glm::perspective(glm::radians(45.0f), (GLfloat)SHADOW_WIDTH / (GLfloat)SHADOW_HEIGHT, near_plane, far_plane); // note that if you use a perspective projection matrix you'll have to change the light position as the current light position isn't enough to reflect the whole scene
+	lightProjection = glm::ortho(-100.0f, 100.0f, -100.0f, 100.0f, near_plane, far_plane);
 	lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
 	lightSpaceMatrix = lightProjection * lightView;
 	// render scene from light's point of view
